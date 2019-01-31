76e61d5f64f18a1872afb75f6a216fae55d94288
michaelwangbh <bwnanjolno@outlook.com>
2019-01-30 23:22:32 -0800

    *process search query

--- Query.md
+++ Query.md
@@ -28,7 +28,7 @@ curl -X PUT "localhost:9200/berry.meta" -H 'Content-Type: application/json' -d'
 
 create twitter.ds_tweet
 
-```aidl
+```
 curl -X PUT "localhost:9200/twitter.ds_tweet" -H 'Content-Type: application/json' -d'
 {
     "mappings" : {
@@ -135,14 +135,13 @@ curl -X POST "localhost:9200/berry.meta/_doc/_bulk" -H 'Content-Type: applicatio
 {"update": {"_id": "twitter.dsCityPopulation"}}
 {"doc": {"name":"twitter.dsCityPopulation","schema":{"typeName":"twitter.typeCityPopulation","dimension":[{"name":"name","isOptional":false,"datatype":"String"},{"name":"cityID","isOptional":false,"datatype":"Number"},{"name":"create_at","isOptional":false,"datatype":"Time"}],"measurement":[{"name":"population","isOptional":false,"datatype":"Number"}],"primaryKey":["cityID"]},"dataInterval":{"start":"1970-01-01T00:00:00.000-0800","end":"2048-01-01T00:00:00.000-0800"},"stats":{"createTime":"2019-01-27T18:06:09.453-0800","lastModifyTime":"2019-01-27T18:06:09.453-0800","lastReadTime":"2019-01-27T18:06:09.453-0800","cardinality":9}}, "doc_as_upsert": true}
 '
-
 ```
 
 # Query 3 (select, time filter, count)
 
 AsterixDB:
 
-```aidl
+```
 select coll_count(
 (select value c from (select value t
 from twitter.ds_tweet t
@@ -152,7 +151,7 @@ where t.`create_at` >= datetime('2019-01-27T15:04:19.068-0800') and t.`create_at
 
 Elasticsearch:
 
-```aidl
+```
 curl -X GET "localhost:9200/twitter.ds_tweet/_doc/_search?filter_path=hits.total" -H 'Content-Type: application/json' -d'                                 
 {
     "query": {
@@ -181,7 +180,7 @@ from twitter.ds_tweet t) as c)
 
 Elasticsearch:
 
-```aidl
+```
 curl -X POST "localhost:9200/twitter.ds_tweet/_search?size=0&filter_path=aggregations.min.value_as_string" -H 'Content-Type: application/json' -d'
 {
     "aggs" : {
@@ -195,12 +194,65 @@ curl -X POST "localhost:9200/twitter.ds_tweet/_search?size=0&filter_path=aggrega
 
 AsterixDB:
 
-```aidl
-
+```
+select `state` as `state`,`day` as `day`,coll_count(g) as `count`
+from twitter.ds_tweet t
+where t.`create_at` >= datetime('2015-01-27T15:04:19.068-0800') and t.`create_at` < datetime('2019-01-28T15:04:19.068-0800') and ftcontains(t.`text`, ['trump'], {'mode':'all'}) and t.`geo_tag`.`stateID` in [ 37,51,24,11,10,34,42,9,44,48,35,4,40,6,20,32,8,49,12,22,28,1,13,45,5,47,21,29,54,17,18,39,19,55,26,27,31,56,41,46,16,30,53,38,25,36,50,33,23,2 ]
+group by t.geo_tag.stateID as `state`,get_interval_start_datetime(interval_bin(t.`create_at`, datetime('1990-01-01T00:00:00.000Z'),  day_time_duration("P1D") )) as `day` group as g
 ```
 
 Elasticsearch:
 
-```aidl
-
+```
+curl -X GET "localhost:9200/twitter.ds_tweet/_search?pretty" -H 'Content-Type: application/json' -d'
+{   
+    "size": 0,
+    "_source": ["id", "state", "day", "text", "geo_tag.stateID"],
+    "query": {
+        "bool": {
+            "must": [
+            {
+                "match": {
+                    "text": {
+                        "query": "trump",
+                        "operator" : "and"
+                    } 
+                }
+            },
+            {
+                "range": {
+                    "create_at": {
+                        "gte": "2015-01-27T15:04:19.068-0800",
+                        "lte": "2019-01-28T15:04:19.068-0800",
+                        "format": "strict_date_time"
+                    }
+                }
+            }],
+            "filter": {
+                "terms": {
+                    "geo_tag.stateID": [1, 2, 4, 5, 6, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 44, 45, 46, 47, 48, 49, 50, 51, 53, 54, 55, 56]
+                }
+            }
+        }
+    },
+    "aggs": {
+        "state": {
+            "terms": {
+                "size": 2147483647,
+                "field": "geo_tag.stateID",
+                "min_doc_count": 1 
+            }, 
+            "aggs": {
+                "day": {
+                    "date_histogram": {
+                        "field": "create_at",
+                        "interval": "day",
+                        "format": "strict_date_time",
+                        "min_doc_count": 1
+                    }
+                }
+            }
+        }
+    }
+}'
 ```
--- cloudberry/zion/src/main/scala/edu/uci/ics/cloudberry/zion/model/impl/ElasticsearchGenerator.scala
+++ cloudberry/zion/src/main/scala/edu/uci/ics/cloudberry/zion/model/impl/ElasticsearchGenerator.scala
@@ -253,53 +253,57 @@ class ElasticsearchGenerator extends IQLGenerator {
   private def parseGroupby(groupOpt: Option[GroupStatement],
                            exprMap: Map[String, FieldExpr],
                            queryAfterAppend: JsObject): (ParsedResult, JsObject) = {
-    (ParsedResult(Seq.empty, exprMap), queryAfterAppend)
+//    (ParsedResult(Seq.empty, exprMap), queryAfterAppend)
 
-//    var result = queryAfterAppend
-//    groupOpt match {
-//      case Some(group) =>
-//        result -= "method"
-//        result += ("method" -> JsString("group by aggregation"))
-//        result += ("size" -> JsNumber(0))
-//        val groupStr = new StringBuilder()
-//        var counter = 0
-//        for (by <- group.bys) {
-//          val as = by.as.getOrElse(by.field).name
-//          val fieldExpr = exprMap(by.field.name).refExpr
-//          if (counter == 0) {
-//            groupStr.append(s"""{"group_by": {""")
-//            result += ("aggr" -> JsString(as))
-//          }
-//          else
-//            groupStr.append(s""","aggs": {"sub_group_by": {""")
-//          by.funcOpt match {
-//            case Some(func) =>
-//              func match {
-//                case bin: Bin => ???
-//                case interval: Interval =>
-//                  val duration = parseIntervalDuration(interval)
-//                  groupStr.append(s""" "date_histogram": {"field": "$fieldExpr", "interval": "$duration", "min_doc_count": 1} """.stripMargin)
-//                case level: Level =>
-//                  val hierarchyField = by.field.asInstanceOf[HierarchyField]
-//                  val field = hierarchyField.levels.find(_._1 == level.levelTag).get._2
-//                  println("Hierarchy inner field name: " + field)
-//                  groupStr.append(s""" "terms": {"field": "${field}", "size": 30000, "min_doc_count": 1} """.stripMargin)
-//                case GeoCellTenth => ???
-//                case GeoCellHundredth => ???
-//                case _ => throw new QueryParsingException(s"unknow function: ${func.name}")
-//              }
-//            case None => ???
-//          }
-//          counter += 1
-//        }
-//        groupStr.append("}" * (counter * 2))
-//        println("groupStr: " + groupStr)
-//        result += ("aggs" -> Json.parse(groupStr.toString))
-//
-//        (ParsedResult(Seq.empty, exprMap), result)
-//      case None =>
-//        (ParsedResult(Seq.empty, exprMap), result)
-//    }
+    var shallowQueryAfterAppend = queryAfterAppend
+    groupOpt match {
+      case Some(group) =>
+        shallowQueryAfterAppend += ("size" -> JsNumber(0))
+        val producedExprs = mutable.LinkedHashMap.newBuilder[String, FieldExpr]
+        var groupStr = new mutable.StringBuilder()
+
+        var counter = 0
+
+        for (i <- group.bys.indices) {
+          var by = group.bys(i)
+          val as = by.as.getOrElse(by.field).name
+          val fieldExpr = exprMap(by.field.name).refExpr
+
+          groupStr.append(s"""{"group_by": {""")
+          shallowQueryAfterAppend += ("aggr" -> JsString(as))
+
+          else
+            groupStr.append(s""","aggs": {"sub_group_by": {""")
+
+          by.funcOpt match {
+            case Some(func) =>
+              func match {
+                case bin: Bin => ???
+                case interval: Interval =>
+                  val duration = parseIntervalDuration(interval)
+                  groupStr.append(s""" "date_histogram": {"field": "$fieldExpr", "interval": "$duration", "min_doc_count": 1} """.stripMargin)
+                case level: Level =>
+                  val hierarchyField = by.field.asInstanceOf[HierarchyField]
+                  val field = hierarchyField.levels.find(_._1 == level.levelTag).get._2
+                  println("Hierarchy inner field name: " + field)
+                  groupStr.append(s""" "terms": {"field": "${field}", "size": 30000, "min_doc_count": 1} """.stripMargin)
+                case GeoCellTenth => ???
+                case GeoCellHundredth => ???
+                case _ => throw new QueryParsingException(s"unknow function: ${func.name}")
+              }
+            case None => ???
+          }
+
+          counter += 1
+        }
+        groupStr.append("}" * (counter * 2))
+        println("groupStr: " + groupStr)
+        result += ("aggs" -> Json.parse(groupStr.toString))
+
+        (ParsedResult(Seq.empty, exprMap), result)
+      case None =>
+        (ParsedResult(Seq.empty, exprMap), result)
+    }
   }
 
   private def parseSelect(selectOpt: Option[SelectStatement],
@@ -437,8 +441,8 @@ class ElasticsearchGenerator extends IQLGenerator {
     }
     else {
       filter.field.dataType match {
-        case DataType.Number => ???
-//          parseNumberRelation(filter, fieldExpr)
+        case DataType.Number =>
+          parseNumberRelation(filter, fieldExpr)
         case DataType.Time =>
           parseTimeRelation(filter, fieldExpr)
         case DataType.Point => ???
@@ -446,8 +450,8 @@ class ElasticsearchGenerator extends IQLGenerator {
         case DataType.Boolean => ???
         case DataType.String => ???
 //          parseStringRelation(filter, fieldExpr)
-        case DataType.Text => ???
-//          parseTextRelation(filter, fieldExpr)
+        case DataType.Text =>
+          parseTextRelation(filter, fieldExpr)
         case DataType.Bag => ???
         case DataType.Hierarchy =>
           throw new QueryParsingException("the Hierarchy type doesn't support any relations.")
@@ -458,24 +462,24 @@ class ElasticsearchGenerator extends IQLGenerator {
     }
   }
 
-//  protected def parseNumberRelation(filter: FilterStatement, fieldExpr: String): String = {
-//    filter.relation match {
-//      case Relation.inRange =>
-//        if (filter.values.size != 2) throw new QueryParsingException(s"relation: ${filter.relation} requires two parameters")
-//        s"""{"range": {"$fieldExpr": {"gte": ${filter.values(0)}, "lt": ${filter.values(1)}}}}""".stripMargin
-//      case Relation.in =>
-//        s"""{"terms": {"$fieldExpr": [${filter.values.mkString(",")}]}}""".stripMargin
-//      case Relation.< =>
-//        s"""{"range": {"$fieldExpr": {"lt": ${filter.values(0)}}}}""".stripMargin
-//      case Relation.> =>
-//        s"""{"range": {"$fieldExpr": {"gt": ${filter.values(0)}}}}""".stripMargin
-//      case Relation.<= =>
-//        s"""{"range": {"$fieldExpr": {"lte": ${filter.values(0)}}}}""".stripMargin
-//      case Relation.>= =>
-//        s"""{"range": {"$fieldExpr": {"gte": ${filter.values(0)}}}}""".stripMargin
-//      case _ => throw new QueryParsingException("no supported parameter for this number in Elasticsearch")
-//    }
-//  }
+  protected def parseNumberRelation(filter: FilterStatement, fieldExpr: String): String = {
+    filter.relation match {
+      case Relation.inRange =>
+        if (filter.values.size != 2) throw new QueryParsingException(s"relation: ${filter.relation} requires two parameters")
+        s"""{"range": {"$fieldExpr": {"gte": ${filter.values(0)}, "lt": ${filter.values(1)}}}}""".stripMargin
+      case Relation.in =>
+        s"""{"terms": {"$fieldExpr": [${filter.values.mkString(",")}]}}""".stripMargin
+      case Relation.< =>
+        s"""{"range": {"$fieldExpr": {"lt": ${filter.values(0)}}}}""".stripMargin
+      case Relation.> =>
+        s"""{"range": {"$fieldExpr": {"gt": ${filter.values(0)}}}}""".stripMargin
+      case Relation.<= =>
+        s"""{"range": {"$fieldExpr": {"lte": ${filter.values(0)}}}}""".stripMargin
+      case Relation.>= =>
+        s"""{"range": {"$fieldExpr": {"gte": ${filter.values(0)}}}}""".stripMargin
+      case _ => throw new QueryParsingException("no supported parameter for this number in Elasticsearch")
+    }
+  }
 
   protected def parseTimeRelation(filter: FilterStatement, fieldExpr: String): String = {
     filter.relation match {
@@ -505,12 +509,12 @@ class ElasticsearchGenerator extends IQLGenerator {
 //  protected def parseStringRelation(filter: FilterStatement, fieldExpr: String): String = {
 //    ""
 //  }
-//
-//  protected def parseTextRelation(filter: FilterStatement, fieldExpr: String): String = {
-//    val words = filter.values.map(w => s"${w.asInstanceOf[String].trim}").mkString("\"", ",", "\"")
-//    s"""{"match": {"${fieldExpr}": {"query": $words, "operator": "and"}}}"""
-//  }
-//
+
+  protected def parseTextRelation(filter: FilterStatement, fieldExpr: String): String = {
+    val words = filter.values.map(w => s"${w.asInstanceOf[String].trim()}").mkString("\"", ",", "\"")
+    s"""{"match": {"${fieldExpr}": {"query": $words, "operator": "and"}}}"""
+  }
+
 //  protected def parseIntervalDuration(interval: Interval): String = {
 //    import TimeUnit._
 //    interval.unit match {
