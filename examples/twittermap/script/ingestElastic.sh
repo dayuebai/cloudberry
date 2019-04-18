3968f90a2b8d03713cc9120d7808d464cad4c23c
dayuebai <dayueb@uci.edu>
2019-04-17 21:43:51 -0700

    Remove unzipped file after ingestion

--- examples/twittermap/script/ingestElastic.sh
+++ examples/twittermap/script/ingestElastic.sh
@@ -3,8 +3,10 @@
 echo start ingestion
 for i in $(ls /home/waans11/twitter/Tweet_2016-*.gz)
 do
-	echo $i
-	gunzip -c $i | ./geotag.sh 7 2>&1 | python ingestElastic.py
+	filename=$(echo $i | cut -f 1 -d '.')
+	echo current file is $filename
+	# gunzip -c $i | ./geotag.sh 7 2>&1 | python ingestElastic.py
+	rm -f $filename
 done
 
 echo finish ingestion.
