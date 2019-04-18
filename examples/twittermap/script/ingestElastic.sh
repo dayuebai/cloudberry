bee89876176588ad12ef69b8d32b5a2076a35a34
dayuebai <dayueb@uci.edu>
2019-04-17 19:58:32 -0700

    Update space to tab

--- examples/twittermap/script/ingestElastic.sh
+++ examples/twittermap/script/ingestElastic.sh
@@ -5,7 +5,7 @@ for i in /home/waans11/twitter/i3-Mar31-2016-us.gz \
 		 /home/waans11/twitter/i3-Mar31-2016-us.gz \
 ; do
 	echo $i
-    gunzip -c $i | ./geotag.sh 7 2>&1 | python ingestElastic.py
+	gunzip -c $i | ./geotag.sh 7 2>&1 | python ingestElastic.py
 done
 
 echo finish ingestion.
